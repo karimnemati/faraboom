911d271b582e43de4749ee231221049ed2f627ab
karimnemati <karim.nemati17@gmail.com>
2020-12-21 17:01:38 +0330

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace farboom.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+           public IActionResult RegisterAgency()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPass()
+        {
+            return View();
+        }
+    }
+}
