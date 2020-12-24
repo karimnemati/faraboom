72a8a0c8acac953e666aaa6f1f541b32050dbe59
karimnemati <karim.nemati17@gmail.com>
2020-12-24 18:46:00 +0330

    New Update

---
+++ Controllers/SinglePageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using faraboom.Models;
+
+namespace faraboom.Controllers
+{
+    public class SinglePageController : Controller
+    {
+
+       
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+
+     public IActionResult Index2()
+        {
+            return View();
+        }
+             public IActionResult Index3()
+        {
+            return View();
+        }
+
+
+
+
+        
+
+    }
+}
