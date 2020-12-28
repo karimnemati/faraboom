c13e06dba0c4391cf41c3fb80331eba7c65d8658
karimnemati <karim.nemati17@gmail.com>
2020-12-28 18:49:30 +0330

    New Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,71 +3,67 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using faraboom.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using faraboom.Models;
 
-namespace faraboom.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace faraboom.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController (ILogger<HomeController> logger) {
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index () {
+            return View ();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        public IActionResult Privacy () {
+            return View ();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        
-        public IActionResult Gallery()
-        {
-            return View();
+        public IActionResult Gallery () {
+            return View ();
         }
-        
-        public IActionResult AboutUs()
-        {
-            return View();
+
+        public IActionResult AboutUs () {
+            return View ();
         }
-        
-        public IActionResult Blog()
-        {
-            return View();
+
+        public IActionResult Blog () {
+            return View ();
         }
-        
-        public IActionResult ContactUs()
-        {
-            return View();
+
+        public IActionResult ContactUs () {
+            return View ();
         }
-        
-        public IActionResult Faq()
-        {
-            return View();
+
+        public IActionResult Faq () {
+            return View ();
         }
-        
-        public IActionResult Ourteam()
-        {
-            return View();
+
+        public IActionResult Ourteam () {
+            return View ();
         }
-          
-        public IActionResult Services()
-        {
-            return View();
+
+        public IActionResult Services () {
+            return View ();
         }
+        public IActionResult MapLogin () {
+            return View ();
+        }
+        public IActionResult Delegate () {
+            return View ();
+        }
+        public IActionResult Managers () {
+            return View ();
+        }
+
     }
 }
--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,24 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using DataLayer.Context;
-using Microsoft.AspNetCore.Mvc;
-using ViewModel.Entities;
+using DataLayer.Entities;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
-using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ViewModel.Entities;
 
-using System.Security.Claims;
-
-
-namespace farboom.Controllers {
+namespace faraboom.Controllers {
     public class LoginController : Controller {
-        public static string eror;
+        public static string eror, massage, NewFileName;
         private readonly ContextHampadco db;
-        public LoginController (ContextHampadco _db) {
+        private readonly IHostingEnvironment _env;
+
+        public LoginController (ContextHampadco _db, IHostingEnvironment env) {
             db = _db;
+            _env = env;
 
         }
 
@@ -27,21 +35,61 @@ namespace farboom.Controllers {
             return View ();
         }
         public IActionResult Login () {
-            if (eror!=null)
-            {
-                ViewBag.eror=eror;
-                eror=null;
-                
+            if (eror != null) {
+                // ViewBag.eror = eror;
+                eror = null;
+
+            }
+
+            if (massage != null) {
+                ViewBag.msg = massage;
+                massage = null;
+
             }
             return View ();
         }
         public IActionResult Register () {
-            
+
             return View ();
         }
         public IActionResult RegisterAgency () {
+
             return View ();
         }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////AddRegisterAgency
+
+        public async Task<IActionResult> AddRegisterAgency (Vm_Register VmReg) {
+
+            //  if (db.Tbl_Registers.Any (a => a.CodeMeli == VmReg.CodeMeli)) {
+            //     massage = "اطلاعات فردی با این کد ملی قبلا ثبت شده است";
+            //     return RedirectToAction ("Login");
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////upload file
+            string FileExtension1 = Path.GetExtension (VmReg.NameFile.FileName);
+            NewFileName = String.Concat (Guid.NewGuid ().ToString (), FileExtension1);
+            var path = $"{_env.WebRootPath}\\fileupload\\{NewFileName}";
+            using (var stream = new FileStream (path, FileMode.Create)) {
+                await VmReg.NameFile.CopyToAsync (stream);
+            }
+            //////////////////////////end upload file 
+
+            Tbl_Register TblReg = new Tbl_Register () {
+                NameFamily = VmReg.NameFamily,
+                CodeMeli = VmReg.CodeMeli,
+                NumberPhone = VmReg.NumberPhone,
+                Password = VmReg.Password,
+                Email = VmReg.Email,
+                Address = VmReg.Address,
+                UploadFile = NewFileName
+
+            };
+            db.Tbl_Registers.Add (TblReg);
+            db.SaveChanges ();
+            massage = "اطلاعات شما با موفقیت ثبت شد لطفا ایمیل خود را چک فرمایید و منتظر تایید از سوی مذیریت باشید";
+            return RedirectToAction ("Login");
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////AddRegisterAgency
 
         public IActionResult ForgotPass () {
             return View ();
@@ -52,26 +100,22 @@ namespace farboom.Controllers {
 
             if (user != null) {
 
-                 var claims = new List<Claim>()
-                {
-                    new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
-                    new Claim(ClaimTypes.Name,"admin")
+                var claims = new List<Claim> () {
+                new Claim (ClaimTypes.NameIdentifier, user.Id.ToString ()),
+                new Claim (ClaimTypes.Name, "admin")
                 };
 
-                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var identity = new ClaimsIdentity (claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                    var principal = new ClaimsPrincipal(identity);
+                var principal = new ClaimsPrincipal (identity);
 
-                    var properties = new AuthenticationProperties
-                    {
-                        IsPersistent = true
-                    };
+                var properties = new AuthenticationProperties {
+                    IsPersistent = true
+                };
 
-                    HttpContext.SignInAsync(principal, properties);
-                    return RedirectToAction("Index", "Home", new { area = "admin" });
-                
+                HttpContext.SignInAsync (principal, properties);
+                return RedirectToAction ("Index", "Home", new { area = "admin" });
 
-                
             } else {
                 eror = "نام کاربری یا رمز عبور شما نادرست است";
                 return RedirectToAction ("Login");
